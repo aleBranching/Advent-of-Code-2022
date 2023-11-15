761a330c78d49eb8a6db20a56692eacb1b170a8a
aleBranching <aleksas773@googlemail.com>
2023-11-15 13:07:51 +0000

    solved but need to rewrite to a cleaner solution

--- day3/index.js
+++ day3/index.js
@@ -1,10 +1,4 @@
 console.log("DAY3");
-// const input = `vJrwpWtwJgWrhcsFMMfFFhFp
-// jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-// PmmdzqPrVvPwwTWBwg
-// wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
-// ttgJtRGJQctTZtZT
-// CrZsJsPPZsGzwwsLwLmpwMDw`;
 
 import * as fs from "fs/promises";
 import { url } from "inspector";
@@ -23,18 +17,53 @@ try {
 
 let spitLines = (input) => {
   let split = input.trim().split("\n");
+
   return split.map((element) => {
     let itsLenght = element.length;
+
     if (itsLenght % 2 !== 0) {
       console.log("THERE IS AN ODD");
     }
 
     let firstHalf = element.slice(0, itsLenght / 2);
+
     let secondHald = element.slice(itsLenght / 2, itsLenght);
 
     return [firstHalf.split(""), secondHald.split("")];
   });
 };
+
+let spitLinesSecond = (input) => {
+  let split = input.trim().split("\n");
+
+  let result = [[]];
+  for (let index = 0; index < split.length / 3; index++) {
+    console.log("the index", index);
+    const element = split[index];
+
+    let ogArrayIndexStart = index * 3;
+    console.log(result[index]);
+
+    result[index].push(
+      split[ogArrayIndexStart].split(""),
+      split[ogArrayIndexStart + 1].split(""),
+      split[ogArrayIndexStart + 2].split("")
+    );
+    if (index < split.length / 3 - 1) {
+      result.push([]);
+    }
+  }
+  return result;
+};
+
+// input = `vJrwpWtwJgWrhcsFMMfFFhFp
+// jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+// PmmdzqPrVvPwwTWBwg
+// wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+// ttgJtRGJQctTZtZT
+// CrZsJsPPZsGzwwsLwLmpwMDw`;
+
+console.log("here", spitLinesSecond(input));
 
 let sameElements = (first, second) =>
   first.reduce(
@@ -43,7 +72,25 @@ let sameElements = (first, second) =>
     []
   );
 
+let sameElementsSecond = (first, second, third) =>
+  first.reduce(
+    (accum, current) =>
+      second.includes(current) && third.includes(current)
+        ? accum.concat(current)
+        : accum,
+    []
+  );
+
+let flattenedTwo = spitLinesSecond(input)
+  .map(([first, second, third]) => [
+    sameElementsSecond(first, second, third).slice(0, 1),
+  ])
+  .map((elem) => elem[0][0]);
+
 let twoD = spitLines(input).map(([first, second]) => [
+  sameElements(first, second).slice(0, 1),
+]);
+let twoDSecond = spitLines(input).map(([first, second]) => [
   sameElements(first, second).slice(0, 1),
 ]);
 
@@ -64,4 +111,10 @@ console.log(
     .reduce((a, b) => a + b)
 );
 
-// spitLines(input).map(([a,b])=> )
+console.log(
+  flattenedTwo
+    .map((elem) =>
+      isLowerCase(elem) ? elem.charCodeAt(0) - 96 : elem.charCodeAt(0) - 38
+    )
+    .reduce((a, b) => a + b)
+);
